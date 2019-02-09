66dfdc9f25e00c6a483bd555c271cf0db3895614
warhir <warhir@gmail.com>
2019-02-09 11:55:54 +0100

    search only offers from all,not from promoted and recommended

--- connectclass.py
+++ connectclass.py
@@ -1,12 +1,13 @@
 import requests
 
 
+
 class Connect:
     def __init__(self, page_link, find, elem_class):
         self.page_link = page_link
         self.find = find
         self.elem_class = elem_class
 
-    def linkConnection(self):
+    def LinkConnection(self):
         return requests.get(self.page_link, timeout=5)
 
--- otodomsearch.py
+++ otodomsearch.py
@@ -1,14 +1,12 @@
-from bs4 import BeautifulSoup
 from connectclass import Connect
+from bs4 import BeautifulSoup
 
 searchlink = "https://www.otodom.pl/sprzedaz/mieszkanie/gdansk/?search%5Bfilter_float_price%3Ato%5D=250000&search%5Bfilter_enum_market%5D%5B0%5D=secondary&search%5Bfilter_float_build_year%3Ato%5D=2016&search%5Bdescription%5D=1&search%5Bdist%5D=10&search%5Bsubregion_id%5D=439&search%5Bcity_id%5D=40&search%5Border%5D=created_at_first%3Adesc"
 
-otodom = Connect(searchlink, "a", "offer-item-title")
-page_response = otodom.linkConnection()
+otodom = Connect(searchlink, "a", "listing_no_promo")
+page_response = otodom.LinkConnection()
 soup = BeautifulSoup(page_response.content, 'html.parser')
-links = soup.find_all(otodom.find, href=True)
+links = soup.find_all(otodom.find, {"data-featured-name" : otodom.elem_class})
 
-for a in links:
-
-    if a.find_all("span", class_=otodom.elem_class, limit=1):
-            print("Found the URL:", a['href'])
+for otodom.find in links:
+    print("Found the URL:", otodom.find['href'])
