1a0570ce7d89a9adf44f97ca9189a3be2e27ad00
warhir <warhir@gmail.com>
2019-02-20 22:56:39 +0100

    scrap and save tp csv source, price, surface, floor and build year from otodom

--- connectresultclass.py
+++ connectresultclass.py
@@ -1,5 +1,24 @@
 from connectclass import Connect
-from bs4 import BeautifulSoup
 
 
 class ConnectResult(Connect):
+    def __init__(self, page_link, find, elem_attribute, elem_class):
+        self.page_link = page_link
+        self.find = find
+        self.elem_attribute = elem_attribute
+        self.elem_class = elem_class
+
+    def search_inside(self, elem_inside, no_class=0):
+        soup = self.CreateStructureSoup()
+        value = soup.find(self.find, {self.elem_attribute: self.elem_class})
+        result = value.find(elem_inside)
+        if not no_class == 0:
+            results = value.find_all(elem_inside)
+            for a in results:
+                b = a
+                a = a.get_text()
+                if a == no_class:
+                    result = b.next_sibling
+                    print(result)
+                    return result
+        return result.get_text()
--- dataresultscrap.py
+++ dataresultscrap.py
@@ -1,15 +1,26 @@
-#from connectresultclass import ConnectResult
+from connectresultclass import ConnectResult
 import pandas as pd
 
 
 #function for scrap data like price, surface from results.csv
 def scrapfromresult():
+    result_list = {'source': [], 'price': [], 'surface': [], 'floor': [], 'build_year': []}
     df = pd.read_csv('results.csv')
-    df.dropna().empty
     for columnsname in df:
         for link in df[columnsname]:
             df.fillna(0, inplace=True)
             if not link == 0:
                 if columnsname == 'otodom':
-                    print(link)
-                    #make a object
+                    #do others properies
+                    price = ConnectResult(link, "li", "class", "param_price").search_inside('strong')
+                    surface = ConnectResult(link, "li", "class", "param_m").search_inside('strong')
+                    floor = ConnectResult(link, "li", "class", "param_floor_no").search_inside('strong')
+                    build_year = ConnectResult(link, "ul", "class", "sub-list").search_inside('strong', "Rok budowy:")
+                    #do a function to save that!!!
+                    result_list['source'].append(link)
+                    result_list['price'].append(price)
+                    result_list['surface'].append(surface)
+                    result_list['floor'].append(floor)
+                    result_list['build_year'].append(build_year)
+        result_list = pd.DataFrame(data=result_list)
+        result_list.to_csv('info.csv', index=False)
