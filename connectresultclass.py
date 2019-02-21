1ecb0b2f828011a23de97fbeb511f3345d4fbd4d
warhir <warhir@gmail.com>
2019-02-21 22:44:45 +0100

    saving otodom with blank cells where readings is null, price and surface for morizon works

--- connectresultclass.py
+++ connectresultclass.py
@@ -2,25 +2,28 @@ from connectclass import Connect
 
 
 class ConnectResult(Connect):
+    # create a parent (in tree) object for search
     def __init__(self, page_link, find, elem_attribute, elem_class):
         self.page_link = page_link
-        self.find = find
-        self.elem_attribute = elem_attribute
-        self.elem_class = elem_class
+        self.find = find  # tag
+        self.elem_attribute = elem_attribute  # attribute
+        self.elem_class = elem_class  # attribute value
 
+    # searching for tag inside parent tag and for text without tag
     def search_inside(self, elem_inside, no_class=0):
         soup = self.CreateStructureSoup()
         value = soup.find(self.find, {self.elem_attribute: self.elem_class})
         result = value.find(elem_inside)
+        # maybe rebuild it with find or find_all ? read bs4 documentation a loop is too heavy
         if not no_class == 0:
             results = value.find_all(elem_inside)
             for a in results:
                 b = a
                 a = a.get_text()
+                print(a)
                 if a == no_class:
                     result = b.next_sibling
-                    #problem with detecting NaN value
-                    if result == 'Rynek:':
-                        result = "NaN"
                     return result
-        return result.get_text()
+        else:
+            result = str(result.get_text())
+            return result
--- dataresultscrap.py
+++ dataresultscrap.py
@@ -17,14 +17,20 @@ def scrapfromresult():
                     build_year = ConnectResult(link, "ul", "class", "sub-list").search_inside('strong', "Rok budowy:")
                     heating = ConnectResult(link, "ul", "class", "sub-list").search_inside('strong', "Ogrzewanie:")
                     rent = ConnectResult(link, "ul", "class", "sub-list").search_inside('strong', "Czynsz:")
+                elif columnsname == 'morizon':
+                    # need solve problem with search inside
+                    price = ConnectResult(link, "li", "class", "paramIconPrice").search_inside('em') # it work
+                    surface = ConnectResult(link, "li", "class", "paramIconLivingArea").search_inside('em') # it work
+                    floor = ConnectResult(link, "section", "class", "params clearfix").search_inside('th', "Piętro: ") # it and down doesnt
+                    build_year = ConnectResult(link, "section", "class", "params clearfix").search_inside('th', "Rok budowy: ")
+                    heating = ConnectResult(link, "section", "class", "params clearfix").search_inside('h3', "Ogrzewanie")
                 result_list = savescrap(result_list, link, price, surface, floor, build_year, heating, rent)
-            elif columnsname == 'morizon':
-                #values for morizon
+
         result_list = pd.DataFrame(data=result_list)
         result_list.to_csv('info.csv', index=False)
 
 
-def savescrap(result_list, link, price, surface, floor='NaN', build_year='NaN', heating='NaN', rent='NaN'):
+def savescrap(result_list, link, price, surface, floor='', build_year='', heating='', rent=''):
     result_list['source'].append(link)
     result_list['price'].append(price)
     result_list['surface'].append(surface)
@@ -33,3 +39,6 @@ def savescrap(result_list, link, price, surface, floor='NaN', build_year='NaN', 
     result_list['heating'].append(heating)
     result_list['rent'].append(rent)
     return result_list
+
+#only for testing
+scrapfromresult()
