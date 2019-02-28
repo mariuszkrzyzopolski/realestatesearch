c478ce611ff5861b964384b488fe2af6f51f884f
warhir <warhir@gmail.com>
2019-02-28 23:00:43 +0100

    i must leave scrap from otodom becasue they rebuild site with unique tags for all ofers. Working freedom, morizon, adresowo, only metrohouse dont work.data from scrap is very limited and not so detailed as i want at start

--- connectresultclass.py
+++ connectresultclass.py
@@ -1,4 +1,5 @@
 from connectclass import Connect
+import bs4
 import re
 
 
@@ -15,20 +16,25 @@ class ConnectResult(Connect):
     # searching for tag inside parent tag and for text without tag
     def search_inside(self, elem_inside, no_class=0, column=0):
         soup = self.CreateStructureSoup()
-
         value = soup.find(self.find, {self.attribute: self.elem_class})
         result = value.find(elem_inside)
         if not no_class == 0:
             create_patern = ""+no_class+"$"
             patern = re.compile(create_patern)
             a = value.find(text=patern)
+            if a is None:
+                return None
+            if column == 'adresowo':
+                return a
             result = a.next_element
             if column == 'morizon':
                 result = result.next_element.get_text()
                 no_class = "\n"+no_class+"\n"
                 result = str(result).replace(no_class, '')
                 result = str(result).replace("\n", '')
-                return result
+            if column == 'freedom':
+                return result.get_text()
+            return result.get_text()
         else:
             result = str(result.get_text())
             return result
--- dataresultscrap.py
+++ dataresultscrap.py
@@ -7,6 +7,7 @@ def scrapfromresult():
     result_list = {'source': [], 'price': [], 'surface': [], 'floor': [], 'build_year': [], 'heating': [], 'rent': []}
     df = pd.read_csv('results.csv')
     df.fillna(0, inplace=True)
+    result_list = pd.DataFrame(data=result_list)
     for columnsname in df:
         for link in df[columnsname]:
             if not link == 0:
@@ -20,29 +21,38 @@ def scrapfromresult():
                     #heating = ConnectResult(link, "ul", "class", "sub-list").search_inside('strong', "Ogrzewanie:")
                     #rent = ConnectResult(link, "ul", "class", "sub-list").search_inside('strong', "Czynsz:")
                 elif columnsname == 'morizon':
-                    # need solve problem with search inside
-                    price = ConnectResult(link, "li", "class", "paramIconPrice").search_inside('em') # it work
-                    surface = ConnectResult(link, "li", "class", "paramIconLivingArea").search_inside('em') # it work
+                    continue
+                    price = ConnectResult(link, "li", "class", "paramIconPrice").search_inside('em')
+                    surface = ConnectResult(link, "li", "class", "paramIconLivingArea").search_inside('em')
                     floor = ConnectResult(link, "section", "class", "params clearfix").search_inside('th', "Piętro: ",columnsname)
                     build_year = ConnectResult(link, "section", "class", "params clearfix").search_inside('th', "Rok budowy: ",columnsname)
                     heating = ConnectResult(link, "section", "class", "params clearfix").search_inside('h3', "Ogrzewanie",columnsname)
                     rent = None
+                elif columnsname == 'adresowo':
+                    continue
+                    price = ConnectResult(link, "span", "class", "price").search_inside('', '', 'default')
+                    surface = ConnectResult(link, "div", "id", "offer-summary").search_inside('', " o powierzchni ")
+                    floor = ConnectResult(link, "div", "id", "offer-summary").search_inside('b', "piętro", columnsname)
+                    build_year = None
+                    heating = None
+                    #heating = ConnectResult(link, "div", "id", "offer-description").search_inside('br', " Ogrzewanie")
+                    rent = None
+                elif columnsname == 'freedom':
+                    price = ConnectResult(link, "div", "class", "columns large-5").search_inside('span')
+                    surface = ConnectResult(link, "div", "class", "columns large-12 data").search_inside("ul" ,'pow. całkowita',"freedom")
+                    floor = ConnectResult(link, "div", "class", "small-12 data").search_inside("div","piętro","freedom")
+                    build_year = None
+                    heating = None
+                    rent = None
                 result_list = savescrap(result_list, link, price, surface, floor, build_year, heating, rent)
-
-        result_list = pd.DataFrame(data=result_list)
-        result_list.to_csv('info.csv', index=False)
+    result_list.to_csv('info.csv', index=False)
 
 
-def savescrap(result_list, link, price, surface, floor='', build_year='', heating='', rent=''):
-    result_list.append([link, price, surface, floor, build_year, heating, rent])
-    print(result_list.head)
-    #result_list['source'].append(link)
-    #result_list['price'].append(price)
-    #result_list['surface'].append(surface)
-    #result_list['floor'].append(floor)
-    #result_list['build_year'].append(build_year)
-    #result_list['heating'].append(heating)
-    #result_list['rent'].append(rent)
+def savescrap(result_list, link, price, surface, floor=None, build_year=None, heating=None, rent=None):
+    tmp_dict = {'source': [link], 'price': [price], 'surface': [surface], 'floor': [floor], 'build_year': [build_year], 'heating': [heating], 'rent': [rent]}
+    tmp_dict = pd.DataFrame(tmp_dict)
+    result_list = pd.concat([result_list, tmp_dict])
+
     return result_list
 
 
